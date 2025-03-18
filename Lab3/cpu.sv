e282a85b4172043387cfbb667184647c29ceac3e
boatboss <soonermh@hotmail.com>
2025-03-18 16:27:17 -0700

    Update ALU

--- Lab3/cpu.sv
+++ Lab3/cpu.sv
@@ -31,8 +31,8 @@ module cpu(
     wire [31:0] readData1, readData2;   
         
     int file1, file2;
-    logic [1:0] AlUsrc;
-    logic [2:0] state;
+    logic [1:0] ALUsrc;
+    logic [2:0] state = 1;
     logic [15:0] cc = 16'd0;
     logic [63:0] pc = 64'd0;
     
@@ -42,14 +42,17 @@ module cpu(
     logic [2:0] func; //func3
     logic [6:0] opcode; //opcode
     
-    logic [31:0] register [31:0];
+    logic [31:0] register [31:0] = '{default: 32'b0};
     
     logic [63:0] IF_ID; //Fetch and Decode carry register
-    logic [135:0] ID_EX; //Decode and Execute carry register
+    logic [136:0] ID_EX; //Decode and Execute carry register
     logic [192:0] EX_MEM; //Execute and Memory carry register
     logic [127:0] MEM_WB; //Memory and Write Back carry register
     
+    logic [31:0] ALUinput;
     
+    alu calc(.A(ID_EX[103:72]), .B(ALUinput), .func3(ID_EX[39:37]), .func7(ID_EX[116:110]), .out(alu_out));
+     
     always@(posedge clk, negedge rst_n)begin
     register[0] <= 0; //Ensure x0 is zero value
         case(state)
@@ -66,7 +69,6 @@ module cpu(
                     pc <= pc + 4; //increment pc for next instruction
                     cc <= cc + 1;
                     state <= 2;
-                    $display("PC = %h", pc); //Display to console
                     //write to file
     //                file1 = $fopen("C:\\CMPE140\\Lab3\\PC.txt", "a");
     //                $fdisplay(file1, "PC = %h", pc);
@@ -88,35 +90,47 @@ module cpu(
                 
                 case(opcode)
                     7'b0010011: begin
-                                    ALUsrc <= 1;
-    //                                i <= imem_insn[31:20];
-                                    i <= IF_ID[63:52];
-    //                                rs <= imem_insn[19:15];
-                                    r1 <= IF_ID[51:47];
-    //                                func <= imem_insn[14:12];
+                                    ALUsrc <= 0;                           
+                                    i <= IF_ID[63:52];                              
+                                    r1 <= IF_ID[51:47];                         
                                     func <= IF_ID[46:44];
-    //                                rd <= imem_insn[11:7];
                                     r2 <= IF_ID[43:39];
                                 end
                 endcase
 
-                $display("%b", IF_ID[63:32]);
-                $display("rs = %b", IF_ID[51:47]);
-                $display("rd = %b", IF_ID[43:39]);
-                
+//                $display("%b", IF_ID[63:32]);
+//                $display("r1 = %1d", IF_ID[51:47]);
+//                $display("r2 = %1d", IF_ID[43:39]);
+
                 i_extend <= { {20{i[11]}}, i}; //sign-extend to 32 bits
-                ID_EX <= {i_extend, register[r1], register[r2], func, r2, IF_ID[31:0]};
+                $display("i_extend: %b", i_extend);
+                ID_EX <= {ALUsrc, i_extend, register[r1], register[r2], func, r2, IF_ID[31:0]};
+                $display("ID/EX: %b" , ID_EX);
+//                alu calc(.A(ID_EX[104:73]), .B(ID_EX[136:105]), .func3(ID_EX[39:37]), .func7(ID_EX[116:110]), .out(alu_out));
+//                $display("%b", ID_EX[104:73]);
+                $display("ID/EX[135:104]:%b", ID_EX[135:104]);
+                $display("ID/EX[103:72]:%b", ID_EX[103:72]);
+                $display("ID/EX[71:40]:%b", ID_EX[71:40]);
+//                $display("%b", ID_EX[39:37]);
+//                $display("%b", ID_EX[116:110]);
+                
                 cc <= cc + 1;
                 state <= 3;
             end
             3: begin
-                //Stage Three - Execute                
+                $display("ID/EX[136]:%b", ID_EX[136]);
+                //Stage Three - Execute    
                 if(!rst_n) begin
                        
                 end 
-                else begin
-                    alu calc(.A(register[rs]), .B(ID_EX[127:95]), .func3(func), .func7(i[11:5]), .out(alu_out));
-                end  
+                if(ID_EX[136] == 0)begin //I-type
+                     ALUinput = ID_EX[135:104];
+                end
+                else if(ID_EX[136] == 1) begin //R-type
+                    ALUinput = ID_EX[71:40];
+                end
+                
+                $display("alu_out = %b", alu_out);                
                 cc <= cc + 1;
                 state <= 4;
             end 
@@ -131,7 +145,7 @@ module cpu(
             end
             5: begin
                 //Stage Five - Write Back
-                $display("x%1d = %h", dmem_addr, dmem_data);
+//                $display("x%1d = %h", dmem_addr, dmem_data);
 //                file2 = $fopen("C:\\CMPE140\\Lab3\\data.txt", "a");
 //                $fdisplay(file2, "x%1d = %h", dmem_addr, dmem_data);
 //                $fclose(file2);
@@ -167,24 +181,22 @@ module alu(
     input logic [2:0] func3,
     input logic [6:0] func7,
     output logic [31:0] out
-);
-
-    logic signed [31:0] B_extend = { {20{B[11]}}, B};
+);    
     
     always@(*) begin
         case(func3)
-            3'b000: out = A + B_extend; //ADDI
-            3'b010: out = (A < B_extend) ? 1:0; //SLTI
-            3'b011: out = (A < B_extend) ? 1:0; //SLTIU
-            3'b100: out = A ^ B_extend; //XORI
-            3'b110: out = A | B_extend; //ORI
-            3'b111: out = A & B_extend; //ANDI
-            3'b001: out = A << B_extend; //SLLI
+            3'b000: out = A + B; //ADDI
+            3'b010: out = (A < B) ? 1:0; //SLTI
+            3'b011: out = (A < B) ? 1:0; //SLTIU
+            3'b100: out = A ^ B; //XORI
+            3'b110: out = A | B; //ORI
+            3'b111: out = A & B; //ANDI
+            3'b001: out = A << B; //SLLI
             3'b101: if(func7 == 7'b0)begin //SRLI
-                        out = A >> B_extend;
+                        out = A >> B;
                     end
                     else if(func7 == 7'b0100000)begin //SRAI
-                        out = A >>> B_extend;
+                        out = A >>> B;
                     end
             default: out = 32'b0;
         endcase
