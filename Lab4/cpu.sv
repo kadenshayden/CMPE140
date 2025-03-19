03c65aa22cd2f4094d852b804d4a24d14d496757
anthony-cuellar <anthony.cuellar@sjsu.edu>
2025-03-19 14:01:54 -0700

    add alu functions

--- Lab4/cpu.sv
+++ Lab4/cpu.sv
@@ -27,13 +27,14 @@ module cpu(
     inout logic [31:0] dmem_data //Write & Read data
     );
     
-    wire [31:0] alu_out;
+    wire [31:0] alu_out, write_data;
     wire [31:0] regData1, regData2; 
-
+      
+        
     int file1, file2;
-    logic ALUsrc, regWrite;
+    logic [1:0] ALUsrc;
     logic [15:0] CC = 16'd0;
-    logic [31:0] PC = 32'd0;
+    logic [63:0] PC = 64'd0;
     
     logic signed [11:0] i; //immediate
     logic signed [31:0] i_extend; //immediate extended
@@ -42,12 +43,11 @@ module cpu(
     logic [6:0] opcode; //opcode
     
     logic [63:0] IF_ID; //Fetch and Decode carry register
-    logic [137:0] ID_EX; //Decode and Execute carry register
-    logic [69:0] EX_MEM; //Execute and Memory carry register
-    logic [69:0] MEM_WB; //Memory and Write Back carry register
+    logic [136:0] ID_EX; //Decode and Execute carry register
+    logic [192:0] EX_MEM; //Execute and Memory carry register
+    logic [127:0] MEM_WB; //Memory and Write Back carry register
     
     logic [31:0] ALUinput;
-    logic [31:0] writeData;
      
     //Stage One - Fetch
     always@(posedge clk, negedge rst_n)begin
@@ -64,18 +64,15 @@ module cpu(
     assign IF_ID = {imem_insn[31:0], PC[31:0]};
     
     //Stage Two - Decode and register read
-    reg_file read(.clk(clk), .regWrite(MEM_WB[37]), .RR1(r1), .RR2(r2), 
-                  .WR(MEM_WB[4:0]), .WD(writeData), .RD1(regData1), 
-                  .RD2(regData2));
+    reg_file read(.clk(clk), .RR1(r1), .RR2(r2), .WR(R2), .WD(write_data),
+                  .RD1(regData1), .RD2(regData2));
     always@(posedge clk, negedge rst_n)begin
         
         //opcode can determine the proper field for immediate value
         opcode = IF_ID[38:32];
         case(opcode)
             7'b0010011: begin
-                            ALUsrc = 0; 
-                            regWrite = 1;
-                            dmem_wen = 0;                          
+                            ALUsrc = 0;                           
                             i = IF_ID[63:52];                              
                             r1 = IF_ID[51:47];                         
                             func = IF_ID[46:44];
@@ -84,10 +81,14 @@ module cpu(
         endcase
 
         i_extend = { {20{i[11]}}, i}; //sign-extend to 32 bits
+        $display("ID/EX: %b" , ID_EX);
+        $display("ID/EX[135:104]:%b", ID_EX[135:104]);
+        $display("ID/EX[103:72]:%b", ID_EX[103:72]);
+        $display("ID/EX[71:40]:%b", ID_EX[71:40]);
+    
     end
     
-    assign ID_EX = {regWrite, ALUsrc, i_extend, regData1, regData2, 
-                    func, r2, IF_ID[31:0]};
+    assign ID_EX = {ALUsrc, i_extend, regData1, regData2, func, r2, IF_ID[31:0]};
     
     //Stage Three - Execute    
     alu calc(.A(ID_EX[103:72]), .B(ALUinput), .func3(ID_EX[39:37]), 
@@ -100,11 +101,9 @@ module cpu(
         else if(ID_EX[136] == 1) begin //R-type
             ALUinput = ID_EX[71:40];
         end
+        
         $display("alu_out = %b", alu_out);  
-        $display("ID_EX: %b", ID_EX);
     end
-    
-    assign  EX_MEM = {ID_EX[137], alu_out, ID_EX[36:32], ID_EX[31:0]};
     
     //Stage Four - Memory Access             
     always@(posedge clk, negedge rst_n)begin
@@ -114,27 +113,18 @@ module cpu(
         else begin
             dmem_wen = 1;
         end
-        
-//        $display("EX_MEM: %b" , EX_MEM);
     end
         
-    assign MEM_WB = {EX_MEM[69], EX_MEM[68:37], EX_MEM[36:32]};
+
  
     //Stage Five - Write Back
     always@(posedge clk, negedge rst_n)begin
-//         $display("MEM_WB: %b" , MEM_WB);
-        if(MEM_WB[37]) begin
-            writeData = MEM_WB[36:5];
-            $display("MEM_WB: %b" , MEM_WB[36:5]);
-            $display("MEM_WB: %b" , MEM_WB[4:0]);
-        end
-        else begin
-            
-        end
-          
+        
+//                   $display("x%1d = %h", dmem_addr, dmem_data);
+//                file2 = $fopen("C:\\CMPE140\\Lab3\\data.txt", "a");
+//                $fdisplay(file2, "x%1d = %h", dmem_addr, dmem_data);
+//                $fclose(file2);
     end
-    
-    
 endmodule
 
 module reg_file(
@@ -143,7 +133,7 @@ module reg_file(
     input logic [31:0] WD,
     output logic [31:0] RD1, RD2
 );
-    logic [31:0] register [31:0] = '{default: 32'b0};; 
+    logic [31:0] register [31:0] = '{default: 32'b0}; 
 
     assign RD1 = register[RR1]; //read data from register 1
     assign RD2 = register[RR2]; //read data from register 2
@@ -154,36 +144,75 @@ module reg_file(
     
 endmodule
 
-module controlUnit(
-    input logic [6:0] opcode
+module controlUnit( // we can try to combine control and alu control into one module
+    input logic [6:0] opcode,
+    output logic branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite,
+    output logic [1:0] ALUOp
     );
     
+    reg [7:0] ctrl;
+    
+    assign {ALUOp[1:0], ALUSrc, Branch, MemRead, MemWrite, RegWrite, MemtoReg} = ctrl;
+    
+    always @(*) begin
+        casez(opcode)
+        //I-type
+            7'b00100011: ctrl = 8'b00101011;
+            
+        //R-type
+            7'b01100011: ctrl = 8'b10000010;
+            
+        endcase
+    end
 endmodule
-module alu(
-    input logic [31:0] A,B,
+
+
+module aluControlUnit(
+    input logic [1:0] ALUOp,
     input logic [2:0] func3,
     input logic [6:0] func7,
+    output logic [4:0] ALUCtrl
+    );
+    
+    always @(*) begin
+        casez({ALUOp, func3})
+        //Itype
+            ({2'b00, 3'b000}): ALUCtrl = 5'b00000; //ADDI
+            ({2'b00, 3'b010}): ALUCtrl = 5'b00001; //SLTI
+            ({2'b00, 3'b011}): ALUCtrl = 5'b00010; //SLTIU
+            ({2'b00, 3'b100}): ALUCtrl = 5'b00011; //XORI
+            ({2'b00, 3'b110}): ALUCtrl = 5'b00100; //ORI
+            ({2'b00, 3'b111}): ALUCtrl = 5'b00101; //ANDI
+            ({2'b00, 3'b001}): ALUCtrl = 5'b00110; //SLLI
+            ({2'b00, 3'b101}): ALUCtrl = (func7[5] == 1 ? 5'b00111 : 5'b01000); //SRAI or SRLI
+        endcase
+    
+    end
+    
+    
+    
+
+endmodule
+
+module alu(
+    input logic [31:0] A,B,
+    input logic [4:0] ALUCtrl, 
     output logic [31:0] out
 );    
     
     always_comb begin
-        case(func3)
-            3'b000: out = A + B; //ADDI
-            3'b010: out = (A < B) ? 1:0; //SLTI
-            3'b011: out = (A < B) ? 1:0; //SLTIU
-            3'b100: out = A ^ B; //XORI
-            3'b110: out = A | B; //ORI
-            3'b111: out = A & B; //ANDI
-            3'b001: out = A << B; //SLLI
-            3'b101: if(func7 == 7'b0)begin //SRLI
-                        out = A >> B;
-                    end
-                    else if(func7 == 7'b0100000)begin //SRAI
-                        out = A >>> B;
-                    end
+        case(ALUCtrl)
+            5'h00: out = A + B; //ADDI
+            5'h01: out = (A < B) ? 1:0; //SLTI
+            5'h02: out = (A < B) ? 1:0; //SLTIU
+            5'h03: out = A ^ B; //XORI
+            5'h04: out = A | B; //ORI
+            5'h05: out = A & B; //ANDI
+            5'h06: out = A << B; //SLLI
+            5'h07: out = A >>> B; //SRAI
+            5'h08: out = A >> B; //SRLI
             default: out = 32'b0;
         endcase
     end
     
-
 endmodule
