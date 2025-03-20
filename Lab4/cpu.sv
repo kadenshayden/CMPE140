bca0fe1a10792333f5ae33928d6d23a922427c39
boatboss <soonermh@hotmail.com>
2025-03-19 19:15:05 -0700

    Update cpu.sv

--- Lab4/cpu.sv
+++ Lab4/cpu.sv
@@ -27,7 +27,6 @@ module cpu(
     inout logic [31:0] dmem_data //Write & Read data
     );
     
-<<<<<<< HEAD
     wire [31:0] alu_out; //Output for ALU
     wire [31:0] regData1, regData2; //Reg file outputs
     wire Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite; //Control Unit Output
@@ -38,16 +37,8 @@ module cpu(
     wire regWriteCarry1, regWriteCarry2;
 
     int file1, file2;
-=======
-    wire [31:0] alu_out, write_data;
-    wire [31:0] regData1, regData2; 
-      
-        
-    int file1, file2;
-    logic [1:0] ALUsrc;
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
     logic [15:0] CC = 16'd0;
-    logic [63:0] PC = 64'd0;
+    logic [31:0] PC = 32'd0;
     
     logic signed [11:0] imm; //immediate
     logic signed [31:0] imm_extend; //immediate extended
@@ -56,19 +47,13 @@ module cpu(
     logic [6:0] func7; 
     logic [6:0] opcode; //opcode
     
-<<<<<<< HEAD
     reg [63:0] IF_ID; //Fetch and Decode carry register
     reg [136:0] ID_EX; //Decode and Execute carry register
     reg [69:0] EX_MEM; //Execute and Memory carry register
     reg [69:0] MEM_WB; //Memory and Write Back carry register
-=======
-    logic [63:0] IF_ID; //Fetch and Decode carry register
-    logic [136:0] ID_EX; //Decode and Execute carry register
-    logic [192:0] EX_MEM; //Execute and Memory carry register
-    logic [127:0] MEM_WB; //Memory and Write Back carry register
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
     
     logic [31:0] ALUinput;
+    logic [31:0] writeData;
      
     //Stage One - Fetch
     always@(posedge clk, negedge rst_n)begin
@@ -83,7 +68,6 @@ module cpu(
         end
     end
     
-<<<<<<< HEAD
     assign IF_ID = {PC[31:0], imem_insn[31:0]}; //Current Bits: 64
     
     //Stage Two - Decode and register read            
@@ -152,50 +136,6 @@ module cpu(
     assign regWriteCarry1 = ID_EX[136];
     
     
-=======
-    //Stage Two - Decode and register read
-    reg_file read(.clk(clk), .RR1(r1), .RR2(r2), .WR(R2), .WD(write_data),
-                  .RD1(regData1), .RD2(regData2));
-    always@(posedge clk, negedge rst_n)begin
-        
-        //opcode can determine the proper field for immediate value
-        opcode = IF_ID[38:32];
-        case(opcode)
-            7'b0010011: begin
-                            ALUsrc = 0;                           
-                            i = IF_ID[63:52];                              
-                            r1 = IF_ID[51:47];                         
-                            func = IF_ID[46:44];
-                            r2 = IF_ID[43:39];
-                        end
-        endcase
-
-        i_extend = { {20{i[11]}}, i}; //sign-extend to 32 bits
-        $display("ID/EX: %b" , ID_EX);
-        $display("ID/EX[135:104]:%b", ID_EX[135:104]);
-        $display("ID/EX[103:72]:%b", ID_EX[103:72]);
-        $display("ID/EX[71:40]:%b", ID_EX[71:40]);
-    
-    end
-    
-    assign ID_EX = {ALUsrc, i_extend, regData1, regData2, func, r2, IF_ID[31:0]};
-    
-    //Stage Three - Execute    
-    alu calc(.A(ID_EX[103:72]), .B(ALUinput), .func3(ID_EX[39:37]), 
-             .func7(ID_EX[116:110]), .out(alu_out));
-     
-    always@(posedge clk, negedge rst_n)begin
-        if(ID_EX[136] == 0)begin //I-type
-             ALUinput = ID_EX[135:104];
-        end
-        else if(ID_EX[136] == 1) begin //R-type
-            ALUinput = ID_EX[71:40];
-        end
-        
-        $display("alu_out = %b", alu_out);  
-    end
-    
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
     //Stage Four - Memory Access             
     always@(posedge clk, negedge rst_n)begin
         if(!rst_n) begin
@@ -207,7 +147,6 @@ module cpu(
             MEM_WB = { regWriteCarry2, writeRegister2, write_alu}; // Current Bits: 38 bits
         end
     end
-<<<<<<< HEAD
     assign write_alu = EX_MEM[31:0];
     assign writeRegister2 = EX_MEM[36:32];
     assign regWriteCarry2 = EX_MEM[37];
@@ -228,19 +167,9 @@ module cpu(
             end
         end
           
-=======
-        
-
- 
-    //Stage Five - Write Back
-    always@(posedge clk, negedge rst_n)begin
-        
-//                   $display("x%1d = %h", dmem_addr, dmem_data);
-//                file2 = $fopen("C:\\CMPE140\\Lab3\\data.txt", "a");
-//                $fdisplay(file2, "x%1d = %h", dmem_addr, dmem_data);
-//                $fclose(file2);
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
     end
+    
+    
 endmodule
 
 module reg_file(
@@ -249,11 +178,7 @@ module reg_file(
     input logic [31:0] WD,
     output logic [31:0] RD1, RD2
 );
-<<<<<<< HEAD
     logic [31:0] register [31:0] = '{default: 32'b0};
-=======
-    logic [31:0] register [31:0] = '{default: 32'b0}; 
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
 
     assign RD1 = register[RR1]; //read data from register 1
     assign RD2 = register[RR2]; //read data from register 2
@@ -266,11 +191,7 @@ endmodule
 
 module controlUnit( // we can try to combine control and alu control into one module
     input logic [6:0] opcode,
-<<<<<<< HEAD
     output logic Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite,
-=======
-    output logic branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite,
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
     output logic [1:0] ALUOp
     );
     
@@ -281,17 +202,10 @@ module controlUnit( // we can try to combine control and alu control into one mo
     always @(*) begin
         casez(opcode)
         //I-type
-<<<<<<< HEAD
             7'b0010011: ctrl = 8'b00101011;
             
         //R-type
             7'b0110011: ctrl = 8'b1000010;
-=======
-            7'b00100011: ctrl = 8'b00101011;
-            
-        //R-type
-            7'b01100011: ctrl = 8'b10000010;
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
             
         endcase
     end
@@ -319,13 +233,6 @@ module aluControlUnit(
         endcase
     
     end
-<<<<<<< HEAD
-=======
-    
-    
-    
-
->>>>>>> 03c65aa22cd2f4094d852b804d4a24d14d496757
 endmodule
 
 module alu(
