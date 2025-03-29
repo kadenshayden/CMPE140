0fb56502b8ae753d03152644267c4ee450af5bbd
boatboss <soonermh@hotmail.com>
2025-03-29 12:24:11 -0700

    Add pipelining and hazard detection

--- Lab5/cpu.sv
+++ Lab5/cpu.sv
@@ -32,6 +32,7 @@ module cpu(
     wire Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite; //Control Unit Output
     wire [1:0] ALUOp; //Control Unit Output
     wire [4:0] ALUCtrl; //ALU Control
+    wire [1:0] forwardA, forwardB;
 
     wire [31:0] data;
     
@@ -42,26 +43,26 @@ module cpu(
     logic signed [11:0] imm; //immediate
     logic signed [31:0] imm_extend; //immediate extended
     logic [4:0] rs1, rs2, rd; //read registers
-    logic [2:0] func3;
-    logic [6:0] func7; 
+    logic [2:0] func3; //Arithmetic functions
+    logic [6:0] func7; //Shift functions
+    logic [4:0] shamt; //Shift amount for SLLI, SRLI, and SRAI
     logic [6:0] opcode; //opcode
     
     reg [63:0] IF_ID; //Fetch and Decode carry register
-    reg [146:0] ID_EX; //Decode and Execute carry register
+    reg [156:0] ID_EX; //Decode and Execute carry register
     reg [69:0] EX_MEM; //Execute and Memory carry register
     reg [69:0] MEM_WB; //Memory and Write Back carry register
     
     wire [63:0] IF_IDwire; //Wire from IF_ID to ID_EX
-    wire [146:0] ID_EXwire; //Wire from ID_EX to EX_MEM
+    wire [156:0] ID_EXwire; //Wire from ID_EX to EX_MEM
     wire [69:0] EX_MEMwire; //Wire from EX_MEM to MEM_WB
     wire [69:0] MEM_WBwire; //Wire from MEM_WB to WB
     
-    logic [31:0] alu_A, alu_B;
+    logic [31:0] alu_A, alu_B, bestAlu_B;
     logic [31:0] writeData;
     logic [4:0] writeReg;
     logic regW;
     
-     
     //Stage One - Fetch
     always@(posedge clk, negedge rst_n)begin
         if(!rst_n) begin
@@ -71,33 +72,33 @@ module cpu(
         else begin         
             dmem_wen = 0;
             imem_addr = PC; //Address of current instruction
+            IF_ID = {PC[31:0], imem_insn[31:0]}; //Current Bits: 64
             PC = PC + 4; //increment pc for next instruction
             CC = CC + 1;
-            IF_ID = {PC[31:0], imem_insn[31:0]}; //Current Bits: 64
+            
 
         end
     end
-    
     assign IF_IDwire = IF_ID;
-    
-    
-    
     
     //Stage Two - Decode and register read            
     controlUnit control(.opcode(IF_IDwire[6:0]), .Branch(Branch), .MemRead(MemRead),
                 .MemtoReg(MemtoReg), .MemWrite(MemWrite), .ALUSrc(ALUSrc), 
                 .RegWrite(RegWrite), .ALUOp(ALUOp));
              
-    reg_file regfile(.clk(clk), .regWrite(regW), .RR1(IF_IDwire[19:15]), .RR2(IF_IDwire[24:20]), 
-                     .WR(writeReg), .WD(writeData), .RD1(regData1), 
+    reg_file regfile(.clk(clk), .regWrite(MEM_WBwire[37]), .RR1(IF_IDwire[19:15]), .RR2(IF_IDwire[24:20]), 
+                     .WR(MEM_WBwire[36:32]), .WD(writeData), .RD1(regData1), 
                      .RD2(regData2));
                 
     //opcode can determine the proper field for immediate value
-//    assign rd = IF_ID[11:7];
-//    assign func3 = IF_ID[14:12];
-//    assign rs1 = IF_ID[19:15];
-//    assign rs2 = IF_ID[24:20];
-//    assign func7 = IF_ID[31:25];
+    //rd = IF_ID[11:7]
+    //func3 = IF_ID[14:12]
+    //rs1 = IF_ID[19:15]
+    //rs2 = IF_ID[24:20]
+    //shamt = IF_ID[24:20]
+    //func7 = IF_ID[31:25]
+    //imm = IF_ID[31:20]
+    
       assign imm = IF_IDwire[31:20];    
       assign imm_extend = { {20{imm[11]}}, imm }; //sign-extend to 32 bits 
 
@@ -106,25 +107,61 @@ module cpu(
            
         end
         else begin    
-            ID_EX = { IF_IDwire[14:12], IF_IDwire[31:25], RegWrite, ALUSrc, ALUOp, IF_IDwire[11:7], imm_extend, regData2, regData1, IF_IDwire[63:32]}; //Current Bits: 147
-            // ID_EX(func3, func7, RegWrite, ALUSrc, ALUOp, rd, imm_extend, regData2, regData1, PC)
+            ID_EX = { IF_IDwire[24:20], IF_IDwire[19:15], IF_IDwire[24:20], IF_IDwire[14:12], IF_IDwire[31:25], 
+                      RegWrite, ALUSrc, ALUOp, IF_IDwire[11:7], imm_extend, regData2, 
+                      regData1, IF_IDwire[63:32]}; //Current Bits: 147
+                      
+            
+            
+            //UPDATE THIS LIST WHEN ADDING TO CARRY REG
+            // ID_EX(shamt, rs1, rs2, func3, func7, RegWrite, ALUSrc, ALUOp, rd, imm_extend, regData2, regData1, PC)
+            
+            // shamt = ID_EX
+            // rs1 = ID_EX[156:152]
+            // rs2 = ID_EX[151:147]
+            // func3 = ID_EX[146:144]
+            // func7 = ID_EX[143:137]
+            // regWrite = ID_EX[136]
+            // ALUSrc = ID_EX[135]
+            // ALUOp = ID_EX[134:133]
+            // rd = ID_EX[132:128]
+            // imm_extend = ID_EX[127:65]
+            // regData2 = ID_EX[95:64]
+            // regData1 = ID_EX[63:32]
+            // PC = ID_EX[31:0]
+            
         end 
     end
-    
     assign ID_EXwire = ID_EX;
     
     
-    //Stage Three - Execute    
+    //Stage Three - Execute 
+    forwardingUnit forward(.EX_MEMregWrite(EX_MEMwire[37]), .MEM_WBregWrite(MEM_WBwire[37]),
+    .EX_MEMregRd(EX_MEMwire[36:32]), .MEM_WBregRd(MEM_WBwire[36:32]), 
+    .rs1(ID_EXwire[156:152]), .rs2(ID_EXwire[151:147]),
+    .forwardA(forwardA), .forwardB(forwardB));
+    
     aluControlUnit aluControl(.ALUOp(ALUOp), .func3(ID_EXwire[146:144]), .func7(ID_EXwire[143:137]), .ALUCtrl(ALUCtrl));
     
-    assign alu_B = (ID_EX[135] == 1) ? ID_EX[127:96] : ID_EX[95:64];
-    alu calc(.A(alu_A), .B(alu_B), .ALUCtrl(ALUCtrl), .out(alu_out));
+    alu calc(.A(alu_A), .B(bestAlu_B), .ALUCtrl(ALUCtrl), .out(alu_out));
     
-    always@(posedge clk)begin 
-        alu_A = regData1;
-        EX_MEM = { ID_EXwire[136], ID_EXwire[132:128], alu_out }; //Holds RegWrite, rd, alu_out
+    assign bestAlu_B = (ID_EXwire[136] == 1) ? ID_EXwire[127:96] : alu_B; //Imm_extend or regData2
+    
+    always@(*) begin
+        case(forwardA)
+            2'b00: alu_A = ID_EXwire[63:32]; // stage 2 (DECODE) regData1
+            2'b10: alu_A = EX_MEMwire[31:0]; // stage 4 (MEMORY) ALU out
+            2'b01: alu_A = writeData;        // stage 5 (WRITE BACK) ALU out
+        endcase
+        case(forwardB)
+            2'b00: alu_B = ID_EXwire[95:64]; // stage 2 (DECODE) regData2
+            2'b10: alu_B = EX_MEMwire[31:0]; // stage 4 (MEMORY) ALU out
+            2'b01: alu_A = writeData;        // stage 5 (WRITE BACK) ALU out
+        endcase
     end
-    
+    always@(posedge clk)begin 
+       EX_MEM = { ID_EXwire[136], ID_EXwire[132:128], alu_out }; //Holds RegWrite, rd, alu_out  
+    end
     assign EX_MEMwire = EX_MEM;
     
     
@@ -133,24 +170,21 @@ module cpu(
             dmem_wen <= 1;
             
             MEM_WB = { EX_MEMwire[37], EX_MEMwire[36:32], EX_MEMwire[31:0]}; // Current Bits: 38 bit
-                                                              //Holds RegWrite, rd, alu_out
+            //Holds RegWrite, rd, alu_out
     end
-    
     assign MEM_WBwire = MEM_WB;
     
     
     //Stage Five - Write Back
-    assign data = (MEM_WB[37] && MEM_WB[31:0]) ? MEM_WB[31:0] : writeData; //change later
-    always@(posedge clk, negedge rst_n)begin
-        if(!rst_n) begin
+    //Conditional checks RegWrite to write to register file
+    always@(MEM_WBwire)begin
+        if(MEM_WBwire[37] && MEM_WBwire[31:0] >= 0)begin
+            writeData = MEM_WB[31:0];
         end
-        else begin
-            writeData = data;
-            regW = MEM_WBwire[37];
-            writeReg = MEM_WBwire[36:32];
-        end
-          
-    end
+        regW = MEM_WBwire[37];
+        writeReg = MEM_WBwire[36:32];
+    end  
+
 
     
 endmodule
@@ -166,10 +200,64 @@ module reg_file(
     assign RD1 = register[RR1]; //read data from register 1
     assign RD2 = register[RR2]; //read data from register 2
     
-    always@(posedge clk) begin
+    always@(regWrite, WR, WD) begin
         if(regWrite) register[WR] <= WD; //write data to destination register
     end 
     
+endmodule
+
+module forwardingUnit(
+    input logic EX_MEMregWrite, MEM_WBregWrite,
+    input logic [4:0] EX_MEMregRd, MEM_WBregRd, rs1, rs2,
+    output logic [1:0] forwardA, forwardB
+);
+    //Determining ALU input A
+    always@(rs1, EX_MEMregRd, MEM_WBregRd, EX_MEMregWrite, MEM_WBregWrite ) begin
+        if (EX_MEMregWrite && (EX_MEMregRd != 0) && (EX_MEMregRd == rs1))begin
+            forwardA = 2'b10;
+        end
+        else if(MEM_WBregWrite && (MEM_WBregRd != 0) && (MEM_WBregRd == rs1)) begin
+            forwardA = 2'b01;
+        end
+        else begin
+            forwardA = 2'b00;
+        end
+    end
+    
+    //Determining ALU input B
+    always@(rs2, EX_MEMregRd, MEM_WBregRd, EX_MEMregWrite, MEM_WBregWrite ) begin
+        if (EX_MEMregWrite && (EX_MEMregRd != 0) && (EX_MEMregRd == rs2))begin
+            forwardB = 2'b10;
+        end
+        else if(MEM_WBregWrite && (MEM_WBregRd != 0) && (MEM_WBregRd == rs2)) begin
+            forwardB = 2'b01;
+        end
+        else begin
+            forwardB = 2'b00;
+        end
+    end
+
+endmodule
+
+//need for load and store
+module hazardDetectionUnit (
+    input logic ID_EXMemRead,
+    input logic [4:0] rs1, rs2, ID_EXregRd, 
+    output logic stall
+    );
+
+always @(ID_EXMemRead, rs1, rs2, ID_EXregRd)
+	begin
+	  if((ID_EXMemRead == 1) && ((ID_EXregRd == rs1)||(ID_EXregRd == rs2)))
+	    begin
+	      stall = 1;
+	    end
+	  else
+	    begin
+	      stall = 0;
+	    end
+	end
+
 endmodule
 
 module controlUnit( // we can try to combine control and alu control into one module
@@ -226,15 +314,15 @@ module alu(
     
     always_comb begin
         case(ALUCtrl)
-            5'h00: out = A + B; //ADDI
-            5'h01: out = (A < B) ? 1:0; //SLTI
-            5'h02: out = (A < B) ? 1:0; //SLTIU
-            5'h03: out = A ^ B; //XORI
-            5'h04: out = A | B; //ORI
-            5'h05: out = A & B; //ANDI
-            5'h06: out = A << B; //SLLI
-            5'h07: out = A >>> B; //SRAI
-            5'h08: out = A >> B; //SRLI
+            0: out = A + B; //ADDI
+            1: out = (A < B) ? 1:0; //SLTI
+            2: out = (A < B) ? 1:0; //SLTIU
+            3: out = A ^ B; //XORI
+            4: out = A | B; //ORI
+            5: out = A & B; //ANDI
+            6: out = A << B; //SLLI
+            7: out = A >>> B; //SRAI
+            8: out = A >> B; //SRLI
             default: out = 32'b0;
         endcase
     end
--- Lab5/tb.sv
+++ Lab5/tb.sv
@@ -51,7 +51,7 @@ cpu dut
 );
     
     // Change to the file you need
-rom #( .addr_width (address_size), .data_width (word_size), .init_file ("addi_hazards.dat") )
+rom #( .addr_width (address_size), .data_width (word_size), .init_file ("i_type.dat") )
 imem (
 .addr(imem_addr),
 .data(imem_insn)
